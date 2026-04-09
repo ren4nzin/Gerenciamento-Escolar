c7732d0dd67292858f2f01b99c1e8e155a3e278b
Renan Coutinho <98997902+ren4nzin@users.noreply.github.com>
2026-04-08 22:26:19 -0300

    adição de atributos no backend e validação de inputs

--- src/main/java/br/com/educacao/educacao/model/Aluno.java
+++ src/main/java/br/com/educacao/educacao/model/Aluno.java
@@ -11,20 +11,32 @@ public class Aluno {
 
     private String nome;
     private Double nota;
+	private String materia;
+    private int semestre
 
     public Aluno() {}
-
-    public Aluno(Long id, String nome, Double nota) {
+      
+	  public Aluno(Long id, String nome, Double nota, String materia) {
+        this.id = id;
+        this.nome = nome;
+       
+    public Aluno(Long id, String nome, Double nota, String materia,int semestre) {
         this.id = id;
         this.nome = nome;
         this.nota = nota;
+		this.materia=materia;
+		this.semestre = semestre;
     }
 
     public Long getId() { return id; }
     public String getNome() { return nome; }
     public Double getNota() { return nota; }
+	public String getMateria(){return materia;}
+	public String getSemestre(){return semestre;}
 
     public void setId(Long id) { this.id = id; }
     public void setNome(String nome) { this.nome = nome; }
     public void setNota(Double nota) { this.nota = nota; }
+	public void setMateria(String materia){this.materia=materia;}
+	public void setSemestre(String semestre){this.semestre=semestre;}
 }
